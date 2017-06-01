bc2340fbac2a4d167de1ee96ed0b430a325b0e18
alexandre.araujo <alexandre.araujo@mv.com.br>
2017-06-01 13:45:32 -0300

    update version

--- tasks/template-convert.js
+++ tasks/template-convert.js
@@ -2,12 +2,14 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var ferrugemjs = require('gulp-ferrugemjs');
 var tsConfig = require("../tsconfig.json").compilerOptions;
+var beautify = require('gulp-jsbeautify');
 
 gulp.task('template',function(){
     return gulp.src([
         "./src/**/*.html"
     ])
     .pipe(ferrugemjs({formatCode:true}))
+    .pipe(beautify({indentSize: 2}))
     .pipe(rename({
         extname: ".html.js"
     }))
